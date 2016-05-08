2490e324ea24d76ed77a8a45c3c3056a66af495c
rhoboro <rhoboro@gmail.com>
2016-05-08 13:08:42 +0900

    About画面の各画面を追加

---
+++ app/src/main/java/jp/pycon/pyconjp2016app/Feature/About/FeedbackActivity.java
@@ -0,0 +1,9 @@
+package jp.pycon.pyconjp2016app.Feature.About;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rhoboro on 5/8/16.
+ */
+public class FeedbackActivity extends AppCompatActivity {
+}
