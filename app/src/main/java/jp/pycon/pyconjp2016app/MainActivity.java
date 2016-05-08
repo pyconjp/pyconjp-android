af3e703fcf24fd466b3747b49a30665821138e0c
rhoboro <rhoboro@gmail.com>
2016-05-08 12:39:49 +0900

    不要なファイルを削除

--- app/src/main/java/jp/pycon/pyconjp2016app/MainActivity.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/MainActivity.java
@@ -39,17 +39,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
-        }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer != null) {
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
