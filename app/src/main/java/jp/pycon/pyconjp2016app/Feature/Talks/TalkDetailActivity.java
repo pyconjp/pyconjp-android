d33d639babf8849758bfab77cc5d02e7d336849d
rhoboro <rhoboro@gmail.com>
2016-07-09 17:34:19 +0900

    トーク詳細画面で戻る処理を実装

--- app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/TalkDetailActivity.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/TalkDetailActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import jp.pycon.pyconjp2016app.R;
 
@@ -22,5 +23,22 @@ public class TalkDetailActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle("Talk Detail");
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
