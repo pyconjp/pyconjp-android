71cee66b684f1a579c1ace0e9b79a3c3404cbf74
rhoboro <rhoboro@gmail.com>
2016-07-09 17:27:06 +0900

    トーク詳細画面のタイトル色を変更

--- app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/TalkDetailActivity.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/TalkDetailActivity.java
@@ -2,7 +2,9 @@ package jp.pycon.pyconjp2016app.Feature.Talks;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import jp.pycon.pyconjp2016app.R;
 
@@ -16,5 +18,9 @@ public class TalkDetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talk_detail);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Talk Detail");
+
     }
 }
