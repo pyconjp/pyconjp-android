dede63d1c37104f3d153b41fd0e9c7a6679ee4be
rhoboro <rhoboro@gmail.com>
2016-07-09 14:07:32 +0900

    トーク一覧のクリック処理を実装

--- app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/Adapter/RealmScheduleAdapter.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/Adapter/RealmScheduleAdapter.java
@@ -2,9 +2,11 @@ package jp.pycon.pyconjp2016app.Feature.Talks.Adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.realm.RealmRecyclerViewAdapter;
 import io.realm.RealmResults;
@@ -25,11 +27,24 @@ public class RealmScheduleAdapter extends RealmRecyclerViewAdapter<RealmSchedule
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new MyViewHolder(inflater.inflate(MyViewHolder.LAYOUT_ID, parent, false));
+        final MyViewHolder holder = new MyViewHolder(inflater.inflate(MyViewHolder.LAYOUT_ID, parent, false));
+        TypedValue val = new TypedValue();
+        if (context.getTheme() != null) {
+            context.getTheme().resolveAttribute(android.R.attr.selectableItemBackground, val, true);
+        }
+        holder.itemView.setBackgroundResource(val.resourceId);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 詳細画面を表示
+                Toast.makeText(view.getContext(), holder.title.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
         RealmScheduleObject obj = getData().get(position);
         holder.obj = obj;
         holder.title.setText(obj.title);
