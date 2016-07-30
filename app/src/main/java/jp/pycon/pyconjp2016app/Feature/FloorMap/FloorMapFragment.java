128a47863a25c973b3762794f5e109606b4d5912
rhoboro <rhoboro@gmail.com>
2016-07-30 23:19:51 +0900

    フロアマップ一通り実装

--- app/src/main/java/jp/pycon/pyconjp2016app/Feature/FloorMap/FloorMapFragment.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Feature/FloorMap/FloorMapFragment.java
@@ -4,12 +4,18 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import jp.pycon.pyconjp2016app.R;
 
@@ -18,7 +24,6 @@ import jp.pycon.pyconjp2016app.R;
  */
 public class FloorMapFragment extends Fragment{
 
-    private String[] FLOORS = {"1階", "2階", "3階"};
     private Context context;
     public FloorMapFragment() {
 
@@ -39,15 +44,80 @@ public class FloorMapFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_floor_map, container, false);
 
+        final List<Floor> floors = new ArrayList<>();
+        floors.add(Floor.F1);
+        floors.add(Floor.F2);
+        floors.add(Floor.F3);
         ListView listView = (ListView)v.findViewById(R.id.list_view);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                FloorMapViewAcitivity.start(context);
+                final Floor floor = floors.get(i);
+                FloorMapViewAcitivity.start(context, floor.getTitleResId(), floor.getImageResId());
             }
         });
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.cell_floor_map, FLOORS);
+        FloorAdapter adapter = new FloorAdapter(getContext(), R.layout.cell_floor_map, floors);
         listView.setAdapter(adapter);
         return v;
+    }
+
+    public static class FloorAdapter extends ArrayAdapter<Floor> {
+
+        private LayoutInflater inflater;
+        private Context context;
+        public FloorAdapter(Context context,
+                            int textViewResourceId,
+                            List<Floor> list) {
+            super(context, textViewResourceId, list);
+            this.context = context;
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Floor floor = getItem(position);
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.cell_floor_map, null);
+            }
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.image_view);
+            imageView.setImageDrawable(ResourcesCompat.getDrawable(context.getResources(), floor.getThumResId(), null));
+            TextView textView = (TextView)convertView.findViewById(R.id.title_text_view);
+            textView.setText(floor.getTitleResId());
+            TextView descTextView = (TextView)convertView.findViewById(R.id.description_text_view);
+            descTextView.setText(floor.getDescResId());
+            return convertView;
+        }
+    }
+
+    public enum Floor {
+        F1(R.drawable.ic_looks_one_black_36dp, R.string.floor_map_01, R.string.floor_map_description_01, R.drawable.floor_map_01),
+        F2(R.drawable.ic_looks_two_black_36dp, R.string.floor_map_02, R.string.floor_map_description_02, R.drawable.floor_map_02),
+        F3(R.drawable.ic_looks_3_black_36dp, R.string.floor_map_03, R.string.floor_map_description_03, R.drawable.floor_map_03);
+        private final int thumResId;
+        private final int imageResId;
+        private final int descResId;
+        private final int titleResId;
+        Floor(int thumResId, int titleResId, int descResId, int imageResId) {
+            this.thumResId = thumResId;
+            this.titleResId = titleResId;
+            this.descResId = descResId;
+            this.imageResId = imageResId;
+        }
+
+        public int getThumResId() {
+            return thumResId;
+        }
+
+        public int getTitleResId() {
+            return titleResId;
+        }
+
+        public int getDescResId() {
+            return descResId;
+        }
+
+        public int getImageResId() {
+            return imageResId;
+        }
     }
 }
--- app/src/main/java/jp/pycon/pyconjp2016app/Feature/FloorMap/FloorMapViewAcitivity.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Feature/FloorMap/FloorMapViewAcitivity.java
@@ -1,10 +1,12 @@
 package jp.pycon.pyconjp2016app.Feature.FloorMap;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import jp.pycon.pyconjp2016app.R;
@@ -13,8 +15,10 @@ import uk.co.senab.photoview.PhotoViewAttacher;
 /**
  * Created by rhoboro on 7/30/16.
  */
-public class FloorMapViewAcitivity extends Activity {
+public class FloorMapViewAcitivity extends AppCompatActivity {
 
+    private static final String BUNDLE_KEY_FLOOR_TITLE_ID = "bundle_key_floor_title_id";
+    private static final String BUNDLE_KEY_FLOOR_IMAGE_ID = "bundle_key_floor_image_id";
     ImageView mImageView;
     PhotoViewAttacher mAttacher;
 
@@ -23,13 +27,32 @@ public class FloorMapViewAcitivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_map_view);
 
+        final int titleResId = getIntent().getIntExtra(BUNDLE_KEY_FLOOR_TITLE_ID, 0);
+        final int imageResId = getIntent().getIntExtra(BUNDLE_KEY_FLOOR_IMAGE_ID, 0);
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setTitle(titleResId);
         mImageView = (ImageView)findViewById(R.id.image_view);
-        mImageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.floor_map_01, null));
+        mImageView.setImageDrawable(ResourcesCompat.getDrawable(getResources(), imageResId, null));
         mAttacher = new PhotoViewAttacher(mImageView);
     }
 
-    public static void start(Context context) {
+    public static void start(Context context, int titleResId, int imageResId) {
         final Intent intent = new Intent(context, FloorMapViewAcitivity.class);
+        intent.putExtra(BUNDLE_KEY_FLOOR_TITLE_ID, titleResId);
+        intent.putExtra(BUNDLE_KEY_FLOOR_IMAGE_ID, imageResId);
         context.startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
