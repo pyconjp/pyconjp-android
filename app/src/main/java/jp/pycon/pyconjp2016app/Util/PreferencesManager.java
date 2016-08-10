41af555268de350707aae3a5c5c743ee76437e0f
rhoboro <rhoboro@gmail.com>
2016-08-10 23:57:33 +0900

    PreferencesManagerにコメントを追加

--- app/src/main/java/jp/pycon/pyconjp2016app/Util/PreferencesManager.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Util/PreferencesManager.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 /**
  * Created by rhoboro on 7/17/16.
+ *
+ * SharedPreferencesへのアクセスを行うクラス
  */
 public class PreferencesManager {
 
@@ -19,6 +21,12 @@ public class PreferencesManager {
     private static final String PREFERENCES_KEY_BOOKMARK_ARRAY = "preferences_key_bookmark_array";
     private static final String PREFERENCES_KEY_MINUTES_BEFORE = "preferences_key_minutes_before";
 
+    /**
+     * 指定されたトークをブックマークに保存します
+     * @param context コンテキスト
+     * @param pk トークのPK
+     * @return 書き込みの成否
+     */
     public static boolean putBookmark(Context context, int pk) {
         SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
         String bookmark = pref.getString(PREFERENCES_KEY_BOOKMARK_ARRAY, "");
@@ -38,6 +46,11 @@ public class PreferencesManager {
         return editor.commit();
     }
 
+    /**
+     * ブックマークに保存されたトークのPK一覧を返します
+     * @param context コンテキスト
+     * @return ブックマークに保存されたトークのPK一覧
+     */
     public static List<Integer> getBookmark(Context context) {
         SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
         String bookmark = pref.getString(PREFERENCES_KEY_BOOKMARK_ARRAY, "");
@@ -56,6 +69,12 @@ public class PreferencesManager {
         return list;
     }
 
+    /**
+     * 指定されたトークをブックマークから削除します
+     * @param context コンテキスト
+     * @param pk ブックマークから削除するトークのPK
+     * @return 削除の成否
+     */
     public static boolean deleteBookmark(Context context, int pk) {
         if (isBookmarkContains(context, pk)) {
             SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
@@ -69,16 +88,33 @@ public class PreferencesManager {
         return true;
     }
 
+    /**
+     * 指定されたトークがブックマークに保存されているかを返します
+     * @param context コンテキスト
+     * @param pk トークのPK
+     * @return ブックマークに保存されている場合はtrue
+     */
     public static boolean isBookmarkContains(Context context, int pk) {
         List<Integer> list = getBookmark(context);
         return list.contains(pk);
     }
 
+    /**
+     * 通知を行う時間はトークの何分前かの設定を保存します
+     * @param context コンテキスト
+     * @return 通知を行う時間はトークの何分前か
+     */
     public static int getMinutesBefore(Context context) {
         SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
         return pref.getInt(PREFERENCES_KEY_MINUTES_BEFORE, Context.MODE_PRIVATE);
     }
 
+    /**
+     * 通知を行う時間はトークの何分前かの設定を保存します
+     * @param context コンテキスト
+     * @param minutes 何分前か
+     * @return 保存の成否
+     */
     public static boolean putMinuteBefore(Context context, int minutes) {
         SharedPreferences pref = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
