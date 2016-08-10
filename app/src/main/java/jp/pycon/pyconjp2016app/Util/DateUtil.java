2f9a42235d9a3f20123d89e83c6c8270a9d177ce
rhoboro <rhoboro@gmail.com>
2016-08-10 23:48:41 +0900

    DateUtilにコメントを追加

--- app/src/main/java/jp/pycon/pyconjp2016app/Util/DateUtil.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Util/DateUtil.java
@@ -18,6 +18,11 @@ public class DateUtil {
     private static final String DISPLAY_DATE_FORMAT = "yyyy/MM/dd HH:mm";
     private static final String DUMMY_DATE = "2016-09-21";
 
+    /**
+     * 13:00の書式の文字列で返します
+     * @param time 13:00:00の書式文字列
+     * @return 13:00の書式文字列
+     */
     @NonNull
     public static String toTimeFormattedString(String time) {
         java.text.DateFormat df1 = new SimpleDateFormat(DATE_FORMAT, Locale.US);
@@ -31,6 +36,13 @@ public class DateUtil {
         return "";
     }
 
+    /**
+     * 2016/09/21 13:00 - 13:30の書式の文字列で返します
+     * @param day 2016-09-21の書式文字列
+     * @param start 13:00:00の書式文字列
+     * @param end 13:30:00の書式文字列
+     * @return 2016/09/21 13:00 - 13:30の書式文字列
+     */
     public static String toStartToEndFormattedString(String day, String start, String end) {
         java.text.DateFormat df1 = new SimpleDateFormat(DATE_FORMAT, Locale.US);
         java.text.DateFormat df2 = new SimpleDateFormat(DISPLAY_DATE_FORMAT, Locale.US);
@@ -47,6 +59,13 @@ public class DateUtil {
         return "";
     }
 
+    /**
+     * 通知をする時間を返します
+     * @param day 通知をするトークのある日
+     * @param start 通知をするトークの開始時刻
+     * @param duration 何分前か
+     * @return 通知をする時間
+     */
     public static long toNotificationMills(String day, String start, int duration) {
         Date date = null;
         SimpleDateFormat df = new SimpleDateFormat(DATE_FORMAT, Locale.JAPAN);
