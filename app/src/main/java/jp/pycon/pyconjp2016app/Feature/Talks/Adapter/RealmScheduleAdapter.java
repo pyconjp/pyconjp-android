aed45ebcdba6680946434d23af06d9379f82474a
rhoboro <rhoboro@gmail.com>
2016-08-28 22:42:07 +0900

    リストにRoomを表示

--- app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/Adapter/RealmScheduleAdapter.java
+++ app/src/main/java/jp/pycon/pyconjp2016app/Feature/Talks/Adapter/RealmScheduleAdapter.java
@@ -1,6 +1,8 @@
 package jp.pycon.pyconjp2016app.Feature.Talks.Adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.View;
@@ -57,10 +59,28 @@ public class RealmScheduleAdapter extends RealmRecyclerViewAdapter<RealmPresenta
         holder.title.setText(obj.title);
         holder.speaker.setText(obj.speakerString());
         holder.start.setText(obj.dispStart);
+        holder.room.setText(obj.rooms);
+        holder.room.setTextColor(ContextCompat.getColor(context, roomColorId(obj.rooms)));
     }
 
     public void setOnClickListener(RealmScheduleAdapterListener listener) {
         this.mListener = listener;
+    }
+
+    private int roomColorId(String room) {
+        int color = R.color.colorRedOrange;
+        if (room.contains("201")) {
+            color = R.color.colorRedOrange;
+        } else if (room.contains("202")) {
+            color = R.color.colorYellowOrange;
+        } else if (room.contains("203")) {
+            color = R.color.colorYellow;
+        } else if (room.contains("204")) {
+            color = R.color.colorYellowGreen;
+        } else if (room.contains("205")) {
+            color = R.color.colorBlueGreen;
+        }
+        return color;
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
@@ -71,12 +91,14 @@ public class RealmScheduleAdapter extends RealmRecyclerViewAdapter<RealmPresenta
         final TextView speaker;
         final TextView start;
         final TextView title;
+        final TextView room;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             speaker = (TextView)itemView.findViewById(R.id.speaker);
             start = (TextView)itemView.findViewById(R.id.start);
             title = (TextView)itemView.findViewById(R.id.title);
+            room = (TextView)itemView.findViewById(R.id.room);
         }
     }
 }
