74bc1516e8d8126967198ec6b8f181918d65ce10
rhoboro <rhoboro@gmail.com>
2016-11-17 23:37:03 +0900

    Dockerでビルドできるように修正

--- Docker/app-build.sh
+++ Docker/app-build.sh
@@ -2,4 +2,4 @@ cd ~ &&
 git clone https://github.com/pyconjp/pyconjp-android.git && 
 cd pyconjp-android && 
 git checkout develop &&
-GOOGLE_MAP_APIKEY_DEBUG='' FABRIC_APIKEY='' ./gradlew clean assembleDebug
+./gradlew clean assembleDebug
